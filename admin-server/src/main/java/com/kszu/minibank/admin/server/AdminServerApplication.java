55ea3741b49238dd1abccf68c2c1bfc699e4345a
kacper <kacper.szubra>
2020-10-25 00:00:00 +0200

    Initial init: config and admin server

---
+++ admin-server/src/main/java/com/kszu/minibank/admin/server/AdminServerApplication.java
@@ -0,0 +1,19 @@
+package com.kszu.minibank.admin.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
